12e09f6a81186c42cd423dcf4682eaf0f697c6b7
Jonathan Harrell <jonathan@worldhqinc.com>
2017-09-14 20:28:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+    "root": true,
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true
+    },
+    "extends": "standard",
+    "plugins": ["compat"],
+    "rules": {
+        "allow-parens": 0,
+        "indent": [1, 4, {"SwitchCase": 1}],
+        "compat/compat": "error"
+    }
+};
